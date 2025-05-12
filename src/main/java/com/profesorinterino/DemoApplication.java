4c349194af7eaba198d6d571fe7e1e2a03ad8722
Arturo <ampalle@gmail.com>
2025-05-12 14:05:59 +0200

    coment

--- src/main/java/com/profesorinterino/DemoApplication.java
+++ src/main/java/com/profesorinterino/DemoApplication.java
@@ -4,6 +4,9 @@ package com.profesorinterino;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Arranca la aplicación en http://localhost:8080/
+ */
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
